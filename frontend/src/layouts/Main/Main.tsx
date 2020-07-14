5a20a2db91cd38b730f03b5bebbe2b50cd4065ac
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-07-13 22:15:25 -0400

    Remove footer

--- frontend/src/layouts/Main/Main.tsx
+++ frontend/src/layouts/Main/Main.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { useMediaQuery } from "@material-ui/core";
 
-import { Sidebar, Topbar, Footer } from "./components";
+import { Sidebar, Topbar } from "./components";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -56,10 +56,7 @@ const Main = (props) => {
         open={shouldOpenSidebar}
         variant={isDesktop ? "persistent" : "temporary"}
       />
-      <main className={classes.content}>
-        {children}
-        <Footer />
-      </main>
+      <main className={classes.content}>{children}</main>
     </div>
   );
 };
